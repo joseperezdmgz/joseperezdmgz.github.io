7cabab2236b4a6d29d890d931b043589a3893e2f
joseperezdmgz <joseperezdmgz@gmail.com>
2023-02-19 21:33:25 +0100

    currentJob Months

--- assets/js/index.js
+++ assets/js/index.js
@@ -30,8 +30,32 @@ $(document).ready( () => {
         $(".burger").removeClass('open');
         $("body").css('overflow','auto');
     })
-    //   $.ajax({url: "index.php",type:  'get', success: function(result){
-    //       console.log(result);
-    //     $("#job_time").text(result);
-    //   }});
-     })
+
+    formatDate=(monthYear)=>{
+        const fecha = monthYear
+        const arrMeses = ['Ene.', 'Feb.', 'Mar.', 'Abr.', 'May.', 'Jun.', 'Jul.', 'Ago.', 'Sep.', 'Oct.', 'Nov.', 'Dic.'];
+        const fechaFormateada = arrMeses[fecha.getMonth()] + ' ' + fecha.getFullYear();
+        return fechaFormateada;
+    }
+    currentJob=()=>{
+        dateIni='Oct. 2022'
+        let fechaInicio = new Date(dateIni);
+        let fechaFin = new Date();
+
+            var years = fechaFin.getFullYear() - fechaInicio.getFullYear();
+            var months = fechaFin.getMonth() - fechaInicio.getMonth();
+          
+            if (months < 0) {
+              months += 12;
+              years -= 1;
+            }
+            if(years==0){
+                $("#current_job").html(dateIni +" - "+ formatDate(fechaFin)+ "· "+months +" meses")
+            }else if(years==1){
+                $("#current_job").html(dateIni +" - "+ formatDate(fechaFin)+ "· "+years +" año"+ " "+months +" meses")
+            }else{
+                $("#current_job").html(dateIni +" - "+ formatDate(fechaFin)+ "· "+years +" años"+ " "+months +" meses")
+            }
+          }
+          currentJob()
+})
--- index.php
+++
@@ -1,9 +0,0 @@
-<?php 		
-
-  $fechaInicio = new DateTime("Oct. 2022");
-  $fechaFin = new DateTime("M. Y");
-  $intervalo = $fechaInicio->diff($fechaFin);
-  $html = $fechaInicio->format("M. Y"	) . " - " . $fechaFin->format("M. Y") . " · " . ($intervalo->y == 0 ?  $intervalo->m." meses" : $intervalo->y ." año " . $intervalo->m." meses");
-  echo $html;
-
-?>
