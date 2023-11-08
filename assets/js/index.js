b2483f7bf102f98afb8738acff48be9ea69799b7
joseperezdmgz <73529207+joseperezdmgz@users.noreply.github.com>
2023-11-08 12:00:13 +0100

    cambio a simple y limpiar codigo

--- assets/js/index.js
+++ assets/js/index.js
@@ -51,13 +51,7 @@ $(document).ready( () => {
               months += 12;
               years -= 1;
             }
-            if(years == 0){
-                $("#current_job").html(dateIni +" - "+ formatDate(fechaFin)+ " · "+months +" meses")
-            }else if(years >= 1){
-                $("#current_job").html(dateIni +" - "+ formatDate(fechaFin)+ " · "+years +" año"+ " "+months +" meses")
-            }else{
-                $("#current_job").html(dateIni +" - "+ formatDate(fechaFin)+ " · "+years +" años"+ " "+months +" meses")
-            }
+            $("#current_job").html(dateIni +" - "+ formatDate(fechaFin)+ " · "+ years + (years == 1 ? (" año"):(" años")) + " "+ months + (months == 1 ?(" mes"):(" meses")))
           }
           currentJob()
 })
