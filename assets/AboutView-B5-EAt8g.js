c3e74274dbadb2b1bad374c43b52d2d9b800af74
joseperezdmgz <joseperezdmgz@gmail.com>
2024-06-15 11:55:48 +0200

    deploy

--- assets/AboutView-B5-EAt8g.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as o,o as t,a as c}from"./index-pf-Htxr1.js";import"https://cdn.jsdelivr.net/gh/orestbida/cookieconsent@3.0.1/dist/cookieconsent.umd.js";const s={},n=c("h1",null,"About",-1),r=[n];function _(a,i){return t(),o("div",null,r)}const u=e(s,[["render",_]]);export{u as default};
