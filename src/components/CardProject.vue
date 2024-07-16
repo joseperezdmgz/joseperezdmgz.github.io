c228e87302eadb7ec9f2b48fa997381e5654e57d
joseperezdmgz <joseperezdmgz@gmail.com>
2024-07-16 21:52:58 +0200

    projects

--- src/components/CardProject.vue
+++ src/components/CardProject.vue
@@ -1,22 +1,18 @@
 <template>
-    <li class="bg-[--bg-box-expansion] rounded flex gap-3 mt-10 overflow-hidden flex-col md:flex-row md:w-full w-4/5 mx-auto p-5">
-      <div>
-        <img :src="idearium_img" class="w-full rounded" alt="">
+    <li v-for="card in props.cards" class="bg-[--bg-box-expansion] rounded flex gap-3 mt-10 overflow-hidden flex-col md:flex-row md:w-full w-4/5 mx-auto p-5">
+      <div class="w-full lg:w-1/4">
+        <img :src="card.img" class="w-full rounded" alt="">
       </div>
-      <div>
-        <p>Hola</p>
-        <p>Hola</p>
-        <p>Hola</p>
-        <p>Hola</p>
-        <p>Hola</p>
-        <p>Hola</p>
-        <p>Hola</p>
-        <p>Hola</p>
-        <p>Hola</p>
+      <div class="w-full lg:w-3/4 md:px-4 relative">
+        <p class="font-bold text-xl pb-4">{{ card.title }}</p>
+        <p class="py-4 md:py-0">{{ card.desc }}</p>
+        <a :href="card.link" target="_blank" class="dark:bg-white dark:text-black text-white bg-black inline-block px-4 md:absolute bottom-0 right-0 rounded">Ver proyecto</a>
       </div>
     </li>  
 </template>
 <script setup>
-import idearium_img from '@/assets/empresas/idearium.webp';
+const props = defineProps({
+    cards: Object,
+  });
 
 </script>
